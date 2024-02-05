c9b8efcb51e3ccf28fcd6cd42495d274d233943d
neoncitylights <neoncitylights@users.noreply.github.com>
2024-02-05 06:43:06 +0000

    deploy: 1ff3f24ef9eda2736719a3c3ba59d3b89597b45e

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["rust_template",{"doc":"{{library}}","t":"H","n":["greet"],"q":[[0,"rust_template"],[1,"alloc::string"]],"d":["Say hi to someone! &lt;3"],"i":[0],"f":"{bd}","c":[],"p":[[1,"str"],[5,"String",1]],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
