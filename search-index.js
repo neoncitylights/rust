19667190c92be7f14688571af27d2029b09f2211
neoncitylights <neoncitylights@users.noreply.github.com>
2024-05-28 16:00:48 +0000

    deploy: 53927a7f658ef9f0d1695db89403fbacfdf64a5a

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["my_crate",{"t":"H","n":["greet"],"q":[[0,"my_crate"],[1,"alloc::string"]],"i":[0],"f":"{{{d{b}}}f}","D":"d","p":[[1,"str"],[1,"reference"],[5,"String",1]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
